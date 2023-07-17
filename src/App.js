1c9c7a746cc4b2074b3a6aae1eb2d7dace03e26c
dipsingh100 <angelicsmile701@gmail.com>
2023-07-17 20:07:01 +0530

    removed uneccessary imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Form from './components/Form';
-import ShowData from './components/ShowData';
 
 function App() {
   return (
     <div className="container">
       <Form/>
-      
     </div>
   );
 }
